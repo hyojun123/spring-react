aa78e9fad56f9e64108422d828ef3771bd1cc876
hyojun.choi <hyojun.choi@weddingbook.com>
2021-11-28 11:22:23 +0900

    .

---
+++ frontend/src/views/TableList.js
@@ -0,0 +1,148 @@
+/*!
+
+=========================================================
+* Now UI Dashboard React - v1.5.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/now-ui-dashboard-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/now-ui-dashboard-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import {React, useState, useEffect} from "react";
+
+// reactstrap components
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardTitle,
+  Table,
+  Row,
+  Col,
+} from "reactstrap";
+
+// core components
+import PanelHeader from "components/PanelHeader/PanelHeader.js";
+
+import { thead, tbody } from "variables/general";
+
+function RegularTables() {
+  const [dataSet,setDataSet] = useState([]);
+  useEffect(() => {
+    console.log(123);
+    let data = [
+      {
+        data: [1,"첫번째 글입니다", "2021-11-24", "효준", 1],
+      },
+      {
+        data: [2,"두번째 글입니다", "2021-11-24", "효준2", 2],
+      },
+      {
+        data: [3,"Sage Rodriguez", "Netherlands", "Baileux", 3],
+      },
+      {
+        data: [4,"Philip Chaney", "Korea, South", "Overland Park", 33],
+      },
+      {
+        data: [5,"Doris Greene", "Malawi", "Feldkirchen in Kärnten", 55],
+      },
+      { className: "", data: ["Mason Porter", "Chile", "Gloucester", "$78,615"] },
+      {
+        className: "table-warning",
+        data: ["Jon Porter", "Portugal", "Gloucester", "$98,615"],
+      },
+    ];
+
+    setDataSet(data);
+  },[]);
+
+  return (
+    <>
+      <PanelHeader size="sm" />
+      <div className="content">
+        <Row>
+          <Col xs={12}>
+            <Card>
+              <CardHeader>
+                <CardTitle tag="h4">게시판</CardTitle>
+              </CardHeader>
+              <CardBody>
+                <Table responsive>
+                  <thead className="text-primary">
+                    <tr>
+                      {thead.map((prop, key) => {
+                        return <th key={key}>{prop}</th>;
+                      })}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {dataSet.map((prop, key) => {
+                      return (
+                        <tr key={key}>
+                          {prop.data.map((prop, key) => {
+                            return <td key={key}>{prop}</td>;
+                          })}
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </Table>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs={12}>
+            <Card className="card-plain">
+              <CardHeader>
+                <CardTitle tag="h4">Table on Plain Background</CardTitle>
+                <p className="category"> Here is a subtitle for this table</p>
+              </CardHeader>
+              <CardBody>
+                <Table responsive>
+                  <thead className="text-primary">
+                    <tr>
+                      {thead.map((prop, key) => {
+                        if (key === thead.length - 1)
+                          return (
+                            <th key={key} className="text-right">
+                              {prop}
+                            </th>
+                          );
+                        return <th key={key}>{prop}</th>;
+                      })}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {tbody.map((prop, key) => {
+                      return (
+                        <tr key={key}>
+                          {prop.data.map((prop, key) => {
+                            if (key === thead.length - 1)
+                              return (
+                                <td key={key} className="text-right">
+                                  {prop}
+                                </td>
+                              );
+                            return <td key={key}>{prop}</td>;
+                          })}
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </Table>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+}
+
+export default RegularTables;
---
+++ src/main/java/com/example/springreact/admin/controller/CustomErrorController.java
@@ -0,0 +1,17 @@
+package com.example.springreact.admin.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+    @RequestMapping("/error")
+    public String handleError() {
+        return "/index.html";
+    }
+
+    public String getErrorPath() {
+        return "/error";
+    }
+}
